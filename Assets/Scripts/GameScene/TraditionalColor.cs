3bc633bebeab31683601b4d6778714d61fc4066b
BlueKoopa <bluekoopa0127@gmail.com>
2022-10-15 07:35:37 +0900

    add ゲームシーンを構成するクラス add テストコード add アセンブリファイル

---
+++ Assets/Scripts/GameScene/TraditionalColor.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static ColorCategory;
+
+public class TraditionalColor
+{
+    private readonly string colorName;
+    private readonly EColorCategory colorCategory;
+    private readonly string colorCode;
+    private readonly string derivation;
+
+    public TraditionalColor(string cn = "赤", EColorCategory cc = EColorCategory.red, string cCode = "#ff0000", string d = "")
+    {
+        colorName = cn;
+        colorCategory = cc;
+        colorCode = cCode;
+        derivation = d;
+    }
+
+    public string GetColorName()
+    {
+        return "";
+    }
+
+    public EColorCategory GetColorCategory()
+    {
+        return EColorCategory.red;
+    }
+
+    public string GetColorCode()
+    {
+        return "";
+    }
+
+    public string GetDerivation()
+    {
+        return "";
+    }
+}
---
+++ Assets/Tests/TColorTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+using static ColorCategory;
+
+public class TColorTest : MonoBehaviour
+{
+    [Test]
+    [TestCase("緑", EColorCategory.green, "#00ff00", "緑色です。")]
+    [TestCase("茶", EColorCategory.brown, "#ffff00", "茶色です。")]
+    public void TestGet(string cn, EColorCategory ecc, string cc, string d)
+    {
+        var a = new TraditionalColor(cn, ecc, cc, d);
+        Assert.That(a.GetColorName(), Is.EqualTo(cn));
+        Assert.That(a.GetColorCategory(), Is.EqualTo(ecc));
+        Assert.That(a.GetColorCode(), Is.EqualTo(cc));
+        Assert.That(a.GetDerivation(), Is.EqualTo(d));
+    }
+}
