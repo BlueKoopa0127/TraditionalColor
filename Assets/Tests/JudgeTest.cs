3ebe95cbc866fe4d104d0129cd4a419de9c9a0f9
BlueKoopa0127 <bluekoopa@me.com>
2022-10-21 16:13:14 +0900

    add JudgeColor,JudgeTest

---
+++ Assets/Tests/JudgeTest.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+using static ColorCategory;
+
+public class JudgeTest : MonoBehaviour
+{
+    [Test]
+    [
+        TestCase(
+            new List<TraditionalColor>(new TraditionalColor("赤"), new TraditionalColor("緑"), new TraditionalColor("白")),
+            new List<TraditionalColor>(new TraditionalColor("緑"), new TraditionalColor("赤"), new TraditionalColor("白")),
+            (1, 1)
+        )
+    ]
+    public void TestGet(List<TraditionalColor> t, List<TraditionalColor> ansT, (int, int) answer)
+    {
+        Judge j = new Judge(ansT);
+        Assert.That(j.Judge(t), Is.EqualTo(answer));
+    }
+}
