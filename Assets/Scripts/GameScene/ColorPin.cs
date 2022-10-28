d145280ecaec42046a042a22addbfb1670e6935c
itochs <halglobe0108@gmail.com>
2022-10-28 16:12:24 +0900

    配置が指定されてないときのばあいわけ

--- Assets/Scripts/GameScene/ColorPin.cs
+++ Assets/Scripts/GameScene/ColorPin.cs
@@ -6,4 +6,10 @@ public class ColorPin
 {
   private readonly EColorCategory colorCategory;
   private readonly int placeID;
+
+  public ColorPin(EColorCategory cc = EColorCategory.red, int id = -1){
+    if(id < 0){
+      // エラーを投げたい気がする
+    }
+  }
 }
