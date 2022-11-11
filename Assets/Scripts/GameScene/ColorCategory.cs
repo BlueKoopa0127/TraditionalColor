08a894c1bb8c36213da37bc5975efb09a627af8b
BlueKoopa0127 <bluekoopa@me.com>
2022-11-11 15:41:03 +0900

    add 大まかな作り

--- Assets/Scripts/GameScene/ColorCategory.cs
+++ Assets/Scripts/GameScene/ColorCategory.cs
@@ -8,4 +8,16 @@ public class ColorCategory
     {
         red, brown, green
     }
+
+    public ColorCategory(EColorCategory e) {
+        //todo eによってファイルを読んでそのカテゴリのTraditionalColorのリストを取得してcolorListに格納する。
+        // スクレイピング待ちではある
+    }
+
+    private List<TraditionalColor> colorList = new List<TraditionalColor>();
+
+    public List<TraditionalColor> RandomChoice(int n) {
+        // todo colorListをランダムシャッフルしてn番目までを返す
+        return null;
+    }
 }
--- Assets/Scripts/GameScene/GameManager.cs
+++ Assets/Scripts/GameScene/GameManager.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static ColorCategory;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        //色のカテゴリを前のシーンから受け取る
+        EColorCategory e = EColorCategory.red;
+        var category = new ColorCategory(e);
+
+        //カテゴリ内から色を10色ランダムに選択する
+        var choiceColor = category.RandomChoice(10);
+
+        //その中から答えを生成する
+        //todo choiceColorの上から4つを選ぶ
+        //todo choiceColorをランダムに入れ替える
+        var answerList = new List<TraditionalColor>();        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //ユーザーが答えを当てる
+        //正誤判定を行う
+        //ユーザーからアクションがあれば
+        //Judge(入力);
+
+        //結果を返す
+        //UIで表示
+
+        //合っていたら終了
+        //Finish();
     }
 }
