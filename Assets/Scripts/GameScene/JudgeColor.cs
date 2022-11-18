78266e54d68a175fe7fccca584eda61656811b6b
itochs <halglobe0108@gmail.com>
2022-11-18 15:32:10 +0900

    コンストラクタの追加

--- Assets/Scripts/GameScene/JudgeColor.cs
+++ Assets/Scripts/GameScene/JudgeColor.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class JudgeColor
 {
+  private List<TraditionalColor> answer;
+  // TODO 判断結果のリストの型，新しくenumのやつで作ってもいいかも
+  private List<string> result;
+
+  JudgeColor(List<TraditionalColor> answer){
+    this.answer = answer;
+    this.result = new List<string>();
+  }
 
 }
