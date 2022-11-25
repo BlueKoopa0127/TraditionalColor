5016719f8d10cb047ba58612d497b6d172d23b09
itochs <halglobe0108@gmail.com>
2022-11-25 14:58:09 +0900

    答えのランダム生成

--- Assets/Scripts/GameScene/GameManager.cs
+++ Assets/Scripts/GameScene/GameManager.cs
@@ -14,11 +14,20 @@ public class GameManager : MonoBehaviour
 
         //カテゴリ内から色を10色ランダムに選択する
         var choiceColor = category.RandomChoice(10);
-
         //その中から答えを生成する
         //todo choiceColorの上から4つを選ぶ
+        // todo colorListをランダムシャッフルしてn番目までを返す
         //todo choiceColorをランダムに入れ替える
-        var answerList = new List<TraditionalColor>();        
+        for(int i = choiceColor.Count -1;i>0;i--){
+            //乱数生成を使ってランダムに取り出す値を決める 
+            int r = UnityEngine.Random.Range(0,i+1);
+            //取り出した値と箱の外の先頭の値を交換する
+            var tmp = choiceColor[i];
+            choiceColor[i] = choiceColor[r];
+            choiceColor[r] = tmp;
+        }
+        
+        var answerList = choiceColor.GetRange(0,4);
     }
 
     // Update is called once per frame
