3a623c277c4b2e8e7d91a7129197ab494575ee41
itochs <halglobe0108@gmail.com>
2022-11-25 14:56:47 +0900

    カテゴリ内からのランダム選択

--- Assets/Scripts/GameScene/ColorCategory.cs
+++ Assets/Scripts/GameScene/ColorCategory.cs
@@ -19,6 +19,14 @@ public class ColorCategory
 
     public List<TraditionalColor> RandomChoice(int n) {
         // todo colorListをランダムシャッフルしてn番目までを返す
-        return null;
+        for(int i = colorList.Count -1;i>0;i--){
+            //乱数生成を使ってランダムに取り出す値を決める 
+            int r = UnityEngine.Random.Range(0, i+1);
+            //取り出した値と箱の外の先頭の値を交換する
+            var tmp = colorList[i];
+            colorList[i] = colorList[r];
+            colorList[r] = tmp;
+        }
+        return colorList.GetRange(0,n);
     }
 }
