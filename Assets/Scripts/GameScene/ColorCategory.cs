1709d16689810bbf27af2b1d41ba62614fa340d2
itochs <halglobe0108@gmail.com>
2022-12-01 18:05:16 +0900

    add category:red, 10 color

--- Assets/Scripts/GameScene/ColorCategory.cs
+++ Assets/Scripts/GameScene/ColorCategory.cs
@@ -10,12 +10,23 @@ public class ColorCategory
         red, brown, green
     }
 
+    private List<TraditionalColor> colorList = new List<TraditionalColor>();
+
     public ColorCategory(EColorCategory e) {
         //todo eによってファイルを読んでそのカテゴリのTraditionalColorのリストを取得してcolorListに格納する。
         // スクレイピング待ちではある
+        var carmine = new TraditionalColor("洋紅色", EColorCategory.red, "#DA003D", "ようこうしょく");
+        var benitou = new TraditionalColor("紅唐", EColorCategory.red, "#D45750", "べにとう");
+        var ginshu = new TraditionalColor("銀朱", EColorCategory.red, "#E24215", "ぎんしゅ");
+        var kobai = new TraditionalColor("紅梅色", EColorCategory.red, "#E86B79", "こうばいいろ");
+        var beniaka = new TraditionalColor("紅赤", EColorCategory.red, "#E5004F", "べにあか");
+        var sinku = new TraditionalColor("真紅", EColorCategory.red, "#AD002D", "しんく");
+        var enji = new TraditionalColor("臙脂色", EColorCategory.red, "#9B003F", "えんじいろ");
+        var zakuro = new TraditionalColor("柘榴色", EColorCategory.red, "#C92E36", "ざくろいろ");
+        var shinshu = new TraditionalColor("真朱", EColorCategory.red, "#D9341D", "しんしゅ");
+        var sangoshu = new TraditionalColor("珊瑚珠色", EColorCategory.red, "#EF454A", "さんごしゅいろ");
     }
 
-    private List<TraditionalColor> colorList = new List<TraditionalColor>();
 
     public List<TraditionalColor> RandomChoice(int n) {
         // todo colorListをランダムシャッフルしてn番目までを返す
