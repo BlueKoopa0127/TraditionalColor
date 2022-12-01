ac8add82057a37cb7ce7680fa4eea081add06243
BlueKoopa0127 <bluekoopa@me.com>
2022-12-01 22:17:10 +0900

    できた

--- Assets/Scripts/GameScene/GameManager.cs
+++ Assets/Scripts/GameScene/GameManager.cs
@@ -8,6 +8,16 @@ public class GameManager : MonoBehaviour
     // Q: こいつらグローバルでいいのか？
     private List<TraditionalColor> userPredict = new List<TraditionalColor>();
     private JudgeColor judge;
+    [SerializeField]
+    private Transform select, selected, answer;
+
+    private int count = 0;
+
+    public void Select(TraditionalColor t)
+    {
+        selected.GetChild(count).GetComponent<TraditionalColor>().Change(t);
+        count++;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -18,29 +28,56 @@ public class GameManager : MonoBehaviour
 
         //カテゴリ内から色を10色ランダムに選択する
         var choiceColor = category.RandomChoice(10);
+        for (int i = 0; i < 10; i++)
+        {
+            select.GetChild(i).GetComponent<TraditionalColor>().Change(choiceColor[i]);
+        }
+
+        // 選択された色のインスタンスの登録
+        for (int i = 0; i < 4; i++)
+        {
+            var t = selected.GetChild(i).GetComponent<TraditionalColor>();
+            userPredict.Add(t);
+        }
+
         //その中から答えを生成する
-        var answer = makeAnswer(choiceColor, 4);
+        var ans = makeAnswer(choiceColor, 4);
+        for (int i = 0; i < 4; i++)
+        {
+            answer.GetChild(i).GetComponent<TraditionalColor>().Change(ans[i]);
+        }
+
         // Q: makeAnserを直接入れてもいいのか？
-        judge = new JudgeColor(answer);
+        judge = new JudgeColor(ans);
     }
 
     // Update is called once per frame
     void Update()
     {
         //ユーザーが答えを当てる
-        
+
         // もしユーザーが予想の確定をしていないなら何もしない
         // continue? return?
 
         // ユーザーの入力の受け取り
         // TODO: userPredict = getUserPredict()的なやつ
-        userPredict = new List<TraditionalColor>();
+        // userPredict = new List<TraditionalColor>();
 
         // 正誤判定を行う
         // Judge.checkHitandBlow(入力);
         // 結果を返す
-        var result = judge.checkHitAndBlow(userPredict);
+        if (count == 4)
+        {
+            var result = judge.checkHitAndBlow(userPredict);
 
+            Debug.Log("Hit : " + result[0] + " Brow : " + result[1]);
+
+            count = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                selected.GetChild(i).GetComponent<TraditionalColor>().White();
+            }
+        }
         // UIで表示
         // 合っていたら終了
         // Finish();
@@ -48,15 +85,16 @@ public class GameManager : MonoBehaviour
 
     private List<TraditionalColor> makeAnswer(List<TraditionalColor> choiceColor, int answerLength)
     {
-        for(int i = choiceColor.Count -1;i>0;i--){
+        for (int i = choiceColor.Count - 1; i > 0; i--)
+        {
             //乱数生成を使ってランダムに取り出す値を決める 
-            int r = UnityEngine.Random.Range(0,i+1);
+            int r = UnityEngine.Random.Range(0, i + 1);
             //取り出した値と箱の外の先頭の値を交換する
             var tmp = choiceColor[i];
             choiceColor[i] = choiceColor[r];
             choiceColor[r] = tmp;
         }
 
-        return choiceColor.GetRange(0,answerLength);
+        return choiceColor.GetRange(0, answerLength);
     }
 }
--- Assets/Scripts/GameScene/TraditionalColor.cs
+++ Assets/Scripts/GameScene/TraditionalColor.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using static ColorCategory;
 
-public class TraditionalColor
+public class TraditionalColor : MonoBehaviour
 {
-    private readonly string colorName;
-    private readonly EColorCategory colorCategory;
-    private readonly string colorCode;
-    private readonly string derivation;
+    [SerializeField]
+    private string colorName;
+    [SerializeField]
+    private EColorCategory colorCategory;
+    [SerializeField]
+    private string colorCode;
+    [SerializeField]
+    private string derivation;
+
+    public void Awake()
+    {
+        Init();
+    }
 
     public TraditionalColor(string cn = "赤", EColorCategory cc = EColorCategory.red, string cCode = "#ff0000", string d = "")
     {
@@ -16,6 +26,32 @@ public class TraditionalColor
         colorCategory = cc;
         colorCode = cCode;
         derivation = d;
+    }
+
+    public void Change(TraditionalColor t)
+    {
+        colorName = t.GetColorName();
+        colorCategory = t.GetColorCategory();
+        colorCode = t.GetColorCode();
+        derivation = t.GetDerivation();
+        Init();
+    }
+
+    private void Init()
+    {
+        var a = GetComponent<Image>();
+        Color b = new Color(0, 0, 0);
+        ColorUtility.TryParseHtmlString(colorCode, out b);
+        a.color = b;
+    }
+
+    public void White()
+    {
+        colorName = "白";
+        colorCategory = EColorCategory.red;
+        colorCode = "#FFFFFF";
+        derivation = "しろ";
+        Init();
     }
 
     public string GetColorName()
@@ -41,5 +77,15 @@ public class TraditionalColor
     public bool Equals(TraditionalColor c)
     {
         return GetColorCode().Equals(c.GetColorCode());
+    }
+
+    public void Print()
+    {
+        Debug.Log(colorName);
+    }
+
+    public void Select()
+    {
+        transform.parent.parent.GetComponent<GameManager>().Select(this);
     }
 }
