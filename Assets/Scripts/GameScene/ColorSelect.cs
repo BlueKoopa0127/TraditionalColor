0321f78a64abd8ac727562a6f9f6f35741876c58
BlueKoopa0127 <bluekoopa@me.com>
2023-01-20 07:34:24 +0900

    button color

--- Assets/Scripts/GameScene/ColorSelect.cs
+++ Assets/Scripts/GameScene/ColorSelect.cs
@@ -19,7 +19,15 @@ public class ColorSelect : MonoBehaviour
             var b = Instantiate(colorSelectButton);
             b.transform.SetParent(transform.GetChild(0), false);
 
-            b.transform.GetChild(0).GetComponent<Text>().text = Translate(Value);
+
+            var (text, color) = Translate(Value);
+            b.transform.GetChild(0).GetComponent<Text>().text = text;
+
+            var i = b.GetComponent<Image>();
+            Color c = new Color(0, 0, 0);
+            ColorUtility.TryParseHtmlString(color, out c);
+            i.color = c;
+
 
             var button = b.GetComponent<Button>();
             button.onClick.AddListener(() => gameManager.SetActive(true));
@@ -29,26 +37,26 @@ public class ColorSelect : MonoBehaviour
         }
     }
 
-    private String Translate(EColorCategory e)
+    private (String, String) Translate(EColorCategory e)
     {
         switch (e)
         {
             case EColorCategory.achromatic:
-                return "白黒系統";
+                return ("白黒系統", "#c8c2be");
             case EColorCategory.blue:
-                return "青系統";
+                return ("青系統", "#0086CC");
             case EColorCategory.brown:
-                return "茶系統";
+                return ("茶系統", "#612E04");
             case EColorCategory.green:
-                return "緑系統";
+                return ("緑系統", "#00a381");
             case EColorCategory.red:
-                return "赤系統";
+                return ("赤系統", "#E2421F");
             case EColorCategory.violet:
-                return "紫系統";
+                return ("紫系統", "#5F414B");
             case EColorCategory.yellow:
-                return "黄系統";
+                return ("黄系統", "#ECE038");
             default:
-                return "";
+                return ("", "");
         }
     }
 }
