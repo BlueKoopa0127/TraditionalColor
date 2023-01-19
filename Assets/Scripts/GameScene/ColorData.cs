0dc16303b674c188832365acd4e7a24e9109a855
itochs <halglobe0108@gmail.com>
2023-01-19 15:44:34 +0900

    別のコミットの混ざったのでその変更を削除

--- Assets/Scripts/GameScene/ColorData.cs
+++ Assets/Scripts/GameScene/ColorData.cs
@@ -9,14 +9,12 @@ public struct ColorData
     public EColorCategory colorCategory { get; private set; }
     public string colorCode { get; private set; }
     public string derivation { get; private set; }
-    public string colorNameRubi { get; private set; }
 
-    public ColorData(string cn = "赤", EColorCategory cc = EColorCategory.red, string cCode = "#ff0000", string d = "", string rubi = "あか")
+    public ColorData(string cn = "赤", EColorCategory cc = EColorCategory.red, string cCode = "#ff0000", string d = "")
     {
         colorName = cn;
         colorCategory = cc;
         colorCode = cCode;
         derivation = d;
-        colorNameRubi = rubi;
     }
 }
