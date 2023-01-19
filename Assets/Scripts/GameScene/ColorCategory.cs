9f879a2950e4949a8c3bb106fb3271654171c428
itochs <halglobe0108@gmail.com>
2023-01-19 13:19:23 +0900

    直打ちの色たちを削除

--- Assets/Scripts/GameScene/ColorCategory.cs
+++ Assets/Scripts/GameScene/ColorCategory.cs
@@ -15,21 +15,6 @@ public class ColorCategory
 
     public ColorCategory(EColorCategory e)
     {
-        //todo eによってファイルを読んでそのカテゴリのTraditionalColorのリストを取得してcolorListに格納する。
-        // スクレイピング待ちではある
-        var carmine = new ColorData("洋紅色", EColorCategory.red, "#DA003D", "ようこうしょく");
-        var benitou = new ColorData("紅唐", EColorCategory.red, "#D45750", "べにとう");
-        var ginshu = new ColorData("銀朱", EColorCategory.red, "#E24215", "ぎんしゅ");
-        var kobai = new ColorData("紅梅色", EColorCategory.red, "#E86B79", "こうばいいろ");
-        var beniaka = new ColorData("紅赤", EColorCategory.red, "#E5004F", "べにあか");
-        var sinku = new ColorData("真紅", EColorCategory.red, "#AD002D", "しんく");
-        var enji = new ColorData("臙脂色", EColorCategory.red, "#9B003F", "えんじいろ");
-        var zakuro = new ColorData("柘榴色", EColorCategory.red, "#C92E36", "ざくろいろ");
-        var shinshu = new ColorData("真朱", EColorCategory.red, "#D9341D", "しんしゅ");
-        var sangoshu = new ColorData("珊瑚珠色", EColorCategory.red, "#EF454A", "さんごしゅいろ");
-
-        colorList = new List<ColorData>() { carmine, benitou, ginshu, kobai, beniaka, sinku, enji, zakuro, shinshu, sangoshu };
-
         string currentDirPath = Environment.CurrentDirectory;
         string relativePathFromcurrentDir = @"./Assets/Data/TraditionalColors.csv";
         string csvFileFullPath = Path.GetFullPath(relativePathFromcurrentDir, currentDirPath);
