4c7016331c976f347a6a7cd1709a46698d928648
itochs <halglobe0108@gmail.com>
2023-01-19 15:23:48 +0900

    rubi用の変数を追加

--- Assets/Scripts/GameScene/ColorData.cs
+++ Assets/Scripts/GameScene/ColorData.cs
@@ -9,12 +9,14 @@ public struct ColorData
     public EColorCategory colorCategory { get; private set; }
     public string colorCode { get; private set; }
     public string derivation { get; private set; }
+    public string colorNameRubi { get; private set; }
 
-    public ColorData(string cn = "赤", EColorCategory cc = EColorCategory.red, string cCode = "#ff0000", string d = "")
+    public ColorData(string cn = "赤", EColorCategory cc = EColorCategory.red, string cCode = "#ff0000", string d = "", string rubi = "あか")
     {
         colorName = cn;
         colorCategory = cc;
         colorCode = cCode;
         derivation = d;
+        colorNameRubi = rubi;
     }
 }
